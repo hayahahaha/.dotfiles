d0f8f3ebad405d4cbf4cc6e2aaeedcf199e1dc34
Duy Hoang <duyhm1998@gmail.com>
2025-10-26 13:26:59 +0700

    chore: clean unused config

--- install.sh
+++ install.sh
@@ -1,18 +1,1 @@
-echo "set up DOTFILES"
-
-echo "Set up tmux"
-sudo apt install tmux
-ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
-
-echo "Set up i3"
-sudo -s ~/.dotfiles/i3/config ~/.config/i3/config
-
-echo "Set up nvim"
-cp -r ~/.dotfiles/nvim ~/.config/nvim
-
-echo "Set up kitty"
-cp -r ~/.dotfiles/kitty ~/.config/kitty
-
-echo "Set up picom"
-sudo -s ~/.dotfiles/picom/picom.conf ~/.config/picom.conf
-
+## Update install script later
